8499fdf5b7df69e51d471e3f1a456622b904848b
Luv Gangwani <luv.gangwani@gmail.com>
2023-04-21 23:34:50 +1000

    Create a connection pool and add new environment variables.

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,12 @@
 import mysql from 'mysql2';
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_ROOT_USERNAME,
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     port: Number(process.env.MYSQL_PORT),
+    connectionLimit: Number(process.env.MYSQL_POOL_CONNECTION_LIMIT)
 });
 
-connection.connect();
+export default pool;
