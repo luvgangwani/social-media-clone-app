aec2267de554116edada719ad9694c0661e11eea
Luv Gangwani <luv.gangwani@gmail.com>
2023-05-04 03:02:03 +1000

    Add a basic request for getting ser by username from client.

--- client/src/components/Login/LoginUsername.tsx
+++ client/src/components/Login/LoginUsername.tsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useState } from 'react'
 import styles from './LoginUsername.module.css';
 import { Link } from 'react-router-dom';
 
 function LoginUsername() {
+
+  const [username, setUsername] = useState('');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const validateUsername = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    fetch('http://localhost:5122/api/v1/users/getUserByUsername', {
+      method: 'POST',
+      body: JSON.stringify({
+        username,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
+
   return (
     <div className={styles.container}>
         <div className={styles.title}>Login</div>
-        <form className={styles.card}>
-            <input type='text' name='username' id='username' placeholder='Username'/>
+        <form className={styles.card} onSubmit={validateUsername}>
+            <input type='text' name='username' id='username' placeholder='Username' onChange={handleInputChange} value={username}/>
             <button type='submit'>Next</button>
             <Link to="/signup" className={styles.link}>Sign up</Link>
         </form>
