3da0509522c89c8a51ace1f62971d4814fa198be
Luv Gangwani <luv.gangwani@gmail.com>
2023-05-04 02:59:21 +1000

    Set server response headers for the node server.

--- server/index.ts
+++ server/index.ts
@@ -12,6 +12,13 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    next();
+});
+
 app.use('/api/v1/posts', posts);
 app.use('/api/v1/users', users);
 app.use('/api/v1/likes', likes);
