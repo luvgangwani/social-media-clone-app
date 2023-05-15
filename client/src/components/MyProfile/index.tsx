4aadafe79f9924b0271c7d5c11487eec38cb9e47
Luv Gangwani <luv.gangwani@gmail.com>
2023-05-15 22:55:24 +1000

    Design the my profile form.

--- client/src/components/MyProfile/index.tsx
+++ client/src/components/MyProfile/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import withAuth from '../../hoc/withAuth';
+import styles from './index.module.css';
 
 function MyProfile() {
   return (
-    <div>My Profile</div>
+    <div className={styles.container}>
+      <div className={styles.header}>My profile</div>
+      <form className={styles.form}>
+        <input type="text" name='firstName' id='firstName' placeholder='First name' />
+        <input type="text" name='lastName' id='lastName' placeholder='Last name' />
+        <span className={styles.username}>Username: </span>
+        <button type='submit' className={styles.update}>Update profile</button>
+      </form>
+    </div>
   )
 }
 
