0148e6e3926a0ded1498e2c870d38b8cf892f341
Luv Gangwani <luv.gangwani@gmail.com>
2023-05-20 17:29:56 +1000

    Move logout under "My account".

--- client/src/Layout/Navigation.tsx
+++ client/src/Layout/Navigation.tsx
@@ -1,28 +1,8 @@
 import React from 'react';
 import styles from './Navigation.module.css';
 import { NavLink } from 'react-router-dom';
-import Modal from '../template/Modal';
-import { useDispatch } from 'react-redux';
-import { setShowModal } from '../redux/modal';
 
 function Navigation() {
-
-  const dispatch = useDispatch();
-
-  const handleLogoutClick = () => {
-    dispatch(setShowModal(true));
-  };
-
-  const handleNoClick = () => {
-    dispatch(setShowModal(false));
-  }
-
-  const handleYesClick = () => {
-    localStorage.removeItem('token');
-    dispatch(setShowModal(false));
-    // TODO: fix bug when navigate() doesn't remove nav links from the header
-    window.location.href = '/';
-  }
 
   return (
     <>
@@ -31,26 +11,7 @@ function Navigation() {
         <NavLink to="/posts">Posts</NavLink>
         <NavLink to="/account">My account</NavLink>
         <NavLink to="/profile">My profile</NavLink>
-        <NavLink
-          to="#"
-          onClick={handleLogoutClick}
-          >Logout</NavLink>
       </nav>
-      <Modal
-        title='Logout'
-      >
-        <span>Are you sure you want to logout?</span>
-        <div className={styles.actions}>
-          <button
-            className={styles.no}
-            onClick={handleNoClick}
-          >No</button>
-          <button
-            className={styles.yes}
-            onClick={handleYesClick}
-            >Yes</button>
-        </div>
-      </Modal>
     </>
   )
 }
--- client/src/components/MyAccount/index.tsx
+++ client/src/components/MyAccount/index.tsx
@@ -1,34 +1,72 @@
 import React from 'react'
 import withAuth from '../../hoc/withAuth';
 import styles from './index.module.css';
+import Modal from '../../template/Modal';
+import { useDispatch } from 'react-redux';
+import { setShowModal } from '../../redux/modal';
 
 function MyAccount() {
+
+  const dispatch = useDispatch();
+
+  const handleLogoutClick = () => {
+    dispatch(setShowModal(true));
+  };
+
+  const handleNoClick = () => {
+    dispatch(setShowModal(false));
+  }
+
+  const handleYesClick = () => {
+    localStorage.removeItem('token');
+    dispatch(setShowModal(false));
+    // TODO: fix bug when navigate() doesn't remove nav links from the header
+    window.location.href = '/';
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <span>My account</span>
+    <>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <span>My account</span>
+        </div>
+        <div className={styles.items}>
+          <div className={styles.item}>
+            <span>Posts you've liked</span>
+            <span>00</span>
+          </div>
+          <div className={styles.item}>
+            <span>Comments</span>
+            <span>00</span>
+          </div>
+          <div className={styles.item}>
+            <span>Friends</span>
+            <span>00</span>
+          </div>
+          <div className={styles.item}>
+            <button onClick={handleLogoutClick}>Logout</button>
+          </div>
+          <div className={styles.item}>
+            <button>Delete account</button>
+          </div>
+        </div>
       </div>
-      <div className={styles.items}>
-        <div className={styles.item}>
-          <span>Posts you've liked</span>
-          <span>00</span>
+      <Modal
+        title='Logout'
+      >
+        <span>Are you sure you want to logout?</span>
+        <div className={styles.actions}>
+          <button
+            className={styles.no}
+            onClick={handleNoClick}
+          >No</button>
+          <button
+            className={styles.yes}
+            onClick={handleYesClick}
+            >Yes</button>
         </div>
-        <div className={styles.item}>
-          <span>Comments</span>
-          <span>00</span>
-        </div>
-        <div className={styles.item}>
-          <span>Friends</span>
-          <span>00</span>
-        </div>
-        <div className={styles.item}>
-          <button>Logout</button>
-        </div>
-        <div className={styles.item}>
-          <button>Delete account</button>
-        </div>
-      </div>
-    </div>
+    </Modal>
+  </>
   )
 }
 
