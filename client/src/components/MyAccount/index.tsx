24d16697e3c6e075e83a969fcdafc6354dd1b461
Luv Gangwani <luv.gangwani@gmail.com>
2023-05-20 16:40:07 +1000

    Design the" My account" page.

--- client/src/components/MyAccount/index.tsx
+++ client/src/components/MyAccount/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import withAuth from '../../hoc/withAuth';
+import styles from './index.module.css';
 
 function MyAccount() {
   return (
-    <div>My Account</div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <span>My account</span>
+      </div>
+      <div className={styles.items}>
+        <div className={styles.item}>
+          <span>Posts you've liked</span>
+          <span>00</span>
+        </div>
+        <div className={styles.item}>
+          <span>Comments</span>
+          <span>00</span>
+        </div>
+        <div className={styles.item}>
+          <span>Friends</span>
+          <span>00</span>
+        </div>
+        <div className={styles.item}>
+          <button>Logout</button>
+        </div>
+        <div className={styles.item}>
+          <button>Delete account</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
