dafa38df99a1596ebf6374f33e5af9758bf347c2
Luv Gangwani <luv.gangwani@gmail.com>
2023-05-22 02:06:31 +1000

    Update search component to make a user search request.

--- client/src/components/Search/index.tsx
+++ client/src/components/Search/index.tsx
@@ -1,9 +1,39 @@
-import React, { ChangeEvent } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import styles from './index.module.css';
+import Setting from '../../setting';
+import { useDispatch } from 'react-redux';
+import { setShowLoader } from '../../redux/loader';
 
 function Search() {
+    const [searchResults, setSearchResults] = useState([]);
+    
+    const dispatch = useDispatch();
+
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.value);
+        const searchQuery = e.target.value;
+
+        if (searchQuery && searchQuery.length >= 2) {
+          dispatch(setShowLoader(true));
+          fetch(Setting.ENDPOINT_SEARCH, {
+            method: 'POST',
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ searchQuery })
+          })
+          .then(response => response.json())
+          .then(({ success, message, data }) => {
+            if (success) setSearchResults(data)
+            else alert(message)
+          })
+          .catch(error => {
+            alert(error.message)
+          })
+          .finally(() => {
+            dispatch(setShowLoader(false))
+          });
+        }
     };
   return (
     <div className={styles.container}>
@@ -16,6 +46,9 @@ function Search() {
             autoFocus
             placeholder='Search'
             onChange={handleInputChange} />
+        {
+          JSON.stringify(searchResults)
+        }
     </div>
   )
 }
