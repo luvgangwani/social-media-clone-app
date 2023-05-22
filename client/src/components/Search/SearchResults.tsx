f5ff400319ca06df2f85688b090a1255d1875cca
Luv Gangwani <luv.gangwani@gmail.com>
2023-05-23 02:51:56 +1000

    Add UI operation for adding a connection. 1

--- client/src/components/Search/SearchResults.tsx
+++ client/src/components/Search/SearchResults.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { MouseEvent } from 'react'
 import styles from './SearchResults.module.css';
 import { SearchResultProps } from '../../types';
+import Setting from '../../setting';
+import { useDispatch } from 'react-redux';
+import { setShowLoader } from '../../redux/loader';
 
 function SearchResults({ searchResults }: SearchResultProps) {
+
+    const dispatch = useDispatch();
+
+    const handleConnectClick = (username: string) => {
+        dispatch(setShowLoader(true));
+        fetch(Setting.ENDPOINT_CONNECTIONS, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                toUsername: username,
+            })
+        })
+        .then(response => response.json())
+        .then(({success, message}) => {
+            if (success) {
+                alert(message);
+            } else {
+                alert(message);
+            }
+        })
+        .catch(error => {
+            alert(error.message)
+        })
+        .finally(() => {
+            dispatch(setShowLoader(false));
+        });
+    };
+
   return (
     <div className={styles.container}>
         {
@@ -12,7 +46,7 @@ function SearchResults({ searchResults }: SearchResultProps) {
                     <div className={styles.name}>{name}</div>
                     <div className={styles.username}>{username}</div>
                 </div>
-                <button className={styles.connect}>Connect</button>
+                <button className={styles.connect} onClick={() => { handleConnectClick(username) }}>Connect</button>
                 </div>
             ))
         }
--- client/src/setting.ts
+++ client/src/setting.ts
@@ -6,14 +6,16 @@ class Setting {
     public static USERS_SERVICE_URL: string = `${this.API_BASE_URL}/users`;
     public static POSTS_SERVICE_URL: string = `${this.API_BASE_URL}/posts`;
     public static PROFILE_SERVICE_URL: string = `${this.API_BASE_URL}/profiles`;
+    public static CONNECTIONS_SERVICE_URL: string = `${this.API_BASE_URL}/connections`;
 
     // Endpoints
     public static ENDPOINT_GET_USER_BY_USERNAME: string = `${this.USERS_SERVICE_URL}/getUserByUsername`;
     public static ENDPOINT_LOGIN: string = `${this.USERS_SERVICE_URL}/login`;
     public static ENDPOINT_SIGNUP: string = `${this.USERS_SERVICE_URL}/register`;
     public static ENDPOINT_SEARCH: string = `${this.USERS_SERVICE_URL}/search`;
-    public static ENDPOINT_POSTS: string = `${this.POSTS_SERVICE_URL}`;
-    public static ENDPOINT_PROFILES: string = `${this.PROFILE_SERVICE_URL}`;
+    public static ENDPOINT_POSTS: string = this.POSTS_SERVICE_URL;
+    public static ENDPOINT_PROFILES: string = this.PROFILE_SERVICE_URL;
+    public static ENDPOINT_CONNECTIONS: string = this.CONNECTIONS_SERVICE_URL
 }
 
 export default Setting;
