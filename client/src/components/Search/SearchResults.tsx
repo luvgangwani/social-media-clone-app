850b43115a40f3657c308b50b2f6fbf5e3737dea
Luv Gangwani <luv.gangwani@gmail.com>
2023-05-24 00:22:23 +1000

    Add logic to display "Disconnect" for already connected user.

--- client/src/components/Search/SearchResults.tsx
+++ client/src/components/Search/SearchResults.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './SearchResults.module.css';
 import { SearchResultProps } from '../../types';
 import Setting from '../../setting';
@@ -7,7 +7,33 @@ import { setShowLoader } from '../../redux/loader';
 
 function SearchResults({ searchResults }: SearchResultProps) {
 
+    const [connectionList, setConnectionList] = useState<string[]>([]);
+
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setShowLoader(true));
+        fetch(Setting.ENDPOINT_GET_CONNECTION_LIST_BY_USERNAME, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            },
+        })
+        .then(response => response.json())
+        .then(({ success, message, data }) => {
+            if (success) {
+                setConnectionList(data);
+            } else {
+                alert(message);
+            }
+        })
+        .catch(error => {
+            alert(error.message);
+        })
+        .finally(() => {
+            dispatch(setShowLoader(false));
+        })
+    }, [dispatch])
+    
 
     const handleConnectClick = (username: string) => {
         dispatch(setShowLoader(true));
@@ -24,6 +50,10 @@ function SearchResults({ searchResults }: SearchResultProps) {
         .then(response => response.json())
         .then(({success, message}) => {
             if (success) {
+                setConnectionList([
+                    ...connectionList,
+                    username,
+                ])
                 alert(message);
             } else {
                 alert(message);
@@ -36,7 +66,7 @@ function SearchResults({ searchResults }: SearchResultProps) {
             dispatch(setShowLoader(false));
         });
     };
-
+  
   return (
     <div className={styles.container}>
         {
@@ -46,7 +76,12 @@ function SearchResults({ searchResults }: SearchResultProps) {
                     <div className={styles.name}>{name}</div>
                     <div className={styles.username}>{username}</div>
                 </div>
-                <button className={styles.connect} onClick={() => { handleConnectClick(username) }}>Connect</button>
+                {
+                    connectionList.includes(username) ?
+                    <button className={styles.connect} onClick={() => { handleConnectClick(username) }}>Disconnect</button>
+                    :
+                    <button className={styles.connect} onClick={() => { handleConnectClick(username) }}>Connect</button>
+                }
                 </div>
             ))
         }
