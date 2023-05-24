d1cd4851448ef49c9b7564db185417c12f17a398
Luv Gangwani <luv.gangwani@gmail.com>
2023-05-25 01:14:38 +1000

    Fix indentation.

--- client/src/Layout/index.tsx
+++ client/src/Layout/index.tsx
@@ -18,25 +18,25 @@ function Layout() {
 
   const fetchConnectionsList = useCallback(() => {
     dispatch(setShowLoader(true));
-        fetch(Setting.ENDPOINT_GET_CONNECTION_LIST_BY_USERNAME, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            },
-        })
-        .then(response => response.json())
-        .then(({ success, message, data }) => {
-            if (success) {
-                dispatch(setConnectionsList(data));
-            } else {
-                alert(message);
-            }
-        })
-        .catch(error => {
-            alert(error.message);
-        })
-        .finally(() => {
-            dispatch(setShowLoader(false));
-        })
+    fetch(Setting.ENDPOINT_GET_CONNECTION_LIST_BY_USERNAME, {
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+        },
+    })
+    .then(response => response.json())
+    .then(({ success, message, data }) => {
+        if (success) {
+            dispatch(setConnectionsList(data));
+        } else {
+            alert(message);
+        }
+    })
+    .catch(error => {
+        alert(error.message);
+    })
+    .finally(() => {
+        dispatch(setShowLoader(false));
+    })
   }, [dispatch])
 
   useEffect(() => {
